7a40b69576c208ad3d72763fa7a049a61afb133c
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2021-07-20 12:58:00 -0500

    Update 4_QUAC_PCoA.R

--- QUAC_analyses/RScripts/4_QUAC_PCoA.R
+++ QUAC_analyses/RScripts/4_QUAC_PCoA.R
@@ -32,13 +32,25 @@ rownames(QUAC_wild_gen@tab) <- QUAC_wild_df$Ind
 rownames(QUAC_garden_gen@tab) <- QUAC_garden_df$Ind 
 
 ##rename populations 
-levels(QUAC_wild_gen@pop) <- unique(QUAC_wild_df$Pop)
+levels(QUAC_wild_gen@pop) <- c("Porter", "Magazine", "Pryor", "Sugar Loaf", "Kessler")
 levels(QUAC_garden_gen@pop) <- unique(QUAC_garden_df$Pop)
 
 ###########################
 ######### PCoA ############
 ###########################
 
+##now run PCA code 
+QUAC_tab <- tab(QUAC_wild_gen, freq=TRUE, NA.method="mean")
+##now create PCA tab
+QUAC_pca <- dudi.pca(QUAC_tab, center=TRUE, scale=FALSE)
+##now look at results
+s.label(QUAC_pca$li)
 
+s.class(QUAC_pca$li, fac=pop(QUAC_wild_gen), col=funky(15))
 
-
+##create color vector 
+QUAC_colors <- c("cadetblue1","cadetblue4","darkgreen","darkorchid","lightcoral")
+##write out 
+s.class(QUAC_pca$li, fac=pop(QUAC_wild_gen),
+        col=QUAC_colors,
+        axesel=FALSE, cstar=0, cpoint=2, clabel = 1)

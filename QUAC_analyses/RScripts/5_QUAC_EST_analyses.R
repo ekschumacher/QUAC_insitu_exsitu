c03c136338060ee1df9e05bbf726a1888dd20972
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2021-07-28 15:06:56 -0500

    Update 5_QUAC_EST_analyses.R

--- QUAC_analyses/RScripts/5_QUAC_EST_analyses.R
+++ QUAC_analyses/RScripts/5_QUAC_EST_analyses.R
@@ -6,6 +6,9 @@
 ##########################
 
 library(adegenet)
+library(diveRsity)
+library(poppr)
+library(hierfstat)
 
 #####################################
 ############ Directories ############
@@ -15,21 +18,35 @@ QUAC_data_files <- "C:\\Users\\eschumacher\\Documents\\GitHub\\QUAC_diversity\\Q
 
 QUAC_analysis_results <- "C:\\Users\\eschumacher\\Documents\\GitHub\\QUAC_diversity\\QUAC_analyses\\Results"
 
-
 ########################################
 ########### Load in Files ##############
 ########################################
 setwd(QUAC_data_files)
+##conversion code if needed
+QUAC_arp_list <- list.files(path = "QUAC_genind\\garden_wild", pattern = ".arp$")
+##now convert 
+for(a in 1:length(QUAC_arp_list)){
+  
+  arp2gen(paste0("QUAC_genind\\garden_wild\\",QUAC_arp_list[[a]]))
+  
+}
 
+####
 QUAC_wild_est_gen <- read.genepop("QUAC_genind\\garden_wild\\QUAC_wild_est.gen", ncode = 3)
 QUAC_wild_est_df <- read.csv("QUAC_data_frames\\garden_wild\\QUAC_wild_est_df.csv")
+##
+QUAC_wild_non_est_gen <- read.genepop("QUAC_genind\\garden_wild\\QUAC_wild_cleaned_non_EST.gen", ncode = 3)
+##load in non-est 
 
 ##rownames 
 rownames(QUAC_wild_est_gen@tab) <- QUAC_wild_est_df$Ind
+rownames(QUAC_wild_non_est_gen@tab) <- QUAC_wild_est_df$Ind
 
 ##name populations 
-levels(QUAC_wild_est_gen) <- unique(QUAC_wild_est_df$Pop)
+levels(QUAC_wild_est_gen@pop) <- unique(QUAC_wild_est_df$Pop)
+levels(QUAC_wild_non_est_gen@pop) <- unique(QUAC_wild_est_df$Pop)
 
+########Wild analyses
 #########################################
 ############ Run Analyses ###############
 #########################################
@@ -45,7 +62,7 @@ write.csv(QUAC_wild_est_allrich_table,"G:\\Shared drives\\Emily_Schumacher\\QUAC
 ##read in table 
 QUAC_est_org <- read.csv("G:\\Shared drives\\Emily_Schumacher\\QUAC_analyses\\QUAC_org.csv")
 ##no significant difference between populations 
-kruskal.test(QUAC_est_org$All_Rich~QUAC_est_org$Pop)
+kruskal.test(QUAC_est_org$All_Rich~as.factor(QUAC_est_org$Pop))
 ##there is a significant difference between loci 
 kruskal.test(QUAC_est_org$All_Rich~QUAC_est_org$Locus)
 ##no difference by pop
@@ -55,18 +72,13 @@ boxplot(QUAC_est_org$All_Rich~QUAC_est_org$Locus, names = names(QUAC_wild_est_ge
 
 ##now try with expected heterozygosity 
 QUAC_poppr_EST <- poppr(QUAC_wild_est_gen)
-QUAC_hexp_df <- cbind(summary(seppop(QUAC_wild_est_gen)$pop1)$Hexp,
-                      summary(seppop(QUAC_wild_est_gen)$pop2)$Hexp,
-                      summary(seppop(QUAC_wild_est_gen)$pop3)$Hexp,
-                      summary(seppop(QUAC_wild_est_gen)$pop4)$Hexp,
-                      summary(seppop(QUAC_wild_est_gen)$pop5)$Hexp)
 
 write.csv(QUAC_hexp_df, "G:\\Shared drives\\Emily_Schumacher\\QUAC_analyses\\QUAC_est_hexp_df.csv")
 
 ##write in hexp 
 QUAC_hexp_org <- read.csv("G:\\Shared drives\\Emily_Schumacher\\QUAC_analyses\\QUAC_est_hexp_org_df.csv")
 ##hexp p-value test-- no difference by pop
-kruskal.test(QUAC_hexp_org$Hexp~QUAC_hexp_org$Pop)
+kruskal.test(QUAC_hexp_org$Hexp~as.factor(QUAC_hexp_org$Pop))
 boxplot(QUAC_hexp_org$Hexp~QUAC_hexp_org$Pop)
 ##hexp 
 kruskal.test(QUAC_hexp_org$Hexp~QUAC_hexp_org$Locus)
@@ -76,4 +88,28 @@ boxplot(QUAC_hexp_org$Hexp~QUAC_hexp_org$Locus)
 ####### Compare non-est with est ############
 #############################################
 
+QUAC_nonest_seppop <- cbind(summary(seppop(QUAC_wild_non_est_gen)$pop1)$Hexp,
+                            summary(seppop(QUAC_wild_non_est_gen)$pop2)$Hexp,
+                            summary(seppop(QUAC_wild_non_est_gen)$pop3)$Hexp,
+                            summary(seppop(QUAC_wild_non_est_gen)$pop4)$Hexp,
+                            summary(seppop(QUAC_wild_non_est_gen)$pop5)$Hexp)
 
+write.csv(QUAC_nonest_seppop, "G:\\Shared drives\\Emily_Schumacher\\QUAC_analyses\\QUAC_nonest_hexp.csv")
+QUAC_hexp_nonest_org <- read.csv("G:\\Shared drives\\Emily_Schumacher\\QUAC_analyses\\QUAC_nonest_hexp_org.csv")
+
+##now do a paired t-test
+wilcox.test(QUAC_hexp_org$Hexp, QUAC_hexp_nonest_org$Hexp, paired = FALSE, alternative = "two.sided")
+
+##calculate allelic richness 
+QUAC_wild_nonest_allrich_table <- allelic.richness(QUAC_wild_non_est_gen)$Ar
+
+##write this out 
+write.csv(QUAC_wild_nonest_allrich_table, "G:\\Shared drives\\Emily_Schumacher\\QUAC_analyses\\QUAC_wild_non_est_allrich.csv")
+QUAC_wild_nonest_allrich <- read.csv("G:\\Shared drives\\Emily_Schumacher\\QUAC_analyses\\QUAC_wild_non_est_allrich_org.csv")
+
+###compare between non est and est 
+wilcox.test(QUAC_est_org$All_Rich, QUAC_wild_nonest_allrich$All_Rich, alterntive = "one.sided")
+pdf("G:\\Shared drives\\Emily_Schumacher\\QUAC_analyses\\QUAC_est_nonest.pdf")
+boxplot(QUAC_est_org$All_Rich, QUAC_wild_nonest_allrich$All_Rich, names = c("ESTs", "Non-ESTs"), 
+        ylim = c(0,15), col = c("darkseagreen1","darkseagreen4"))
+dev.off()

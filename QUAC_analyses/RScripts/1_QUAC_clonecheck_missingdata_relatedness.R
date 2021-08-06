a0b30a41f74b41ea2fa864f763f6925cbc91a77c
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2021-08-06 14:38:05 -0500

    updating r script with better naming and consistency

--- QUAC_analyses/RScripts/1_QUAC_clonecheck_missingdata_relatedness.R
+++ QUAC_analyses/RScripts/1_QUAC_clonecheck_missingdata_relatedness.R
@@ -23,50 +23,48 @@ QUAC_analysis_results <- "C:\\Users\\eschumacher\\Documents\\GitHub\\QUAC_divers
 ##############################################
 ############# Convert to Genind ##############
 ##############################################
-
+##set working directory to load in data files 
 setwd(QUAC_data_files)
 
-##convert to genind 
-arp2gen(paste0(QUAC_data_files, "\\QUAC_genind\\quac_allpop.arp"))
+##convert QUAC all pop document into genind if needed
+#arp2gen(paste0(QUAC_data_files, "\\QUAC_genind\\quac_allpop.arp"))
 
 ##now read in genind file 
-QUAC_allpop_gen <- read.genepop(paste0(QUAC_data_files, "\\QUAC_genind\\quac_allpop.gen"), ncode = 3)
+QUAC_allpop_gen <- read.genepop("QUAC_genind\\quac_allpop.gen", ncode = 3)
  
 ##load files to label population names and indvidual names 
-QUAC_allpop_df <- read.csv(paste0(QUAC_data_files, "\\QUAC_data_frames\\QUAC_allpop_df.csv"))
+QUAC_allpop_df <- read.csv("QUAC_data_frames\\QUAC_allpop.csv")
 
 ##rename individuals in the genind file
 rownames(QUAC_allpop_gen@tab) <- QUAC_allpop_df$ID
 
 ##rename population names 
 QUAC_popnames <- unique(QUAC_allpop_df$POP)
-
 levels(QUAC_allpop_gen@pop) <- QUAC_popnames
 
-#load in relatedness data frame 
-QUAC_rel_df <- read.csv("QUAC_data_frames\\QUAC_allpop_clean.csv")
+##load document for relatedness analysis 
+QUAC_rel_df <- read.csv("QUAC_data_frames\\QUAC_rel_df.csv")
 
 #######################################################################
 ############# Clone check, relatedness, and missing data ##############
 #######################################################################
-
 ####run clone check 
-##convert to clone test
+##convert to clone check format
 QUAC_geneclone <- as.genclone(QUAC_allpop_gen)
-##give id to all MLGs
+##identify multi-locus genotypes (non-clones)
 QUAC_gen_id <- mlg.id(QUAC_geneclone)
 ##function to pull out all clone lists
 QUAC_clone_index <- which(sapply(QUAC_gen_id,function(x) length(x)>1))
 ##now remove clones from the matrix 
 QUAC_noclones <- clonecorrect(QUAC_geneclone)
 ##convert back to genind 
-QUAC_genind_nocl <- genclone2genind(QUAC_noclones)
+QUAC_genind_nocl <- genclone2genind(QUAC_noclones) ##left with 456 individuals 
 
 ##remove individuals with too much missing data 
 QUAC_genind_nomd <- missingno(QUAC_genind_nocl, type = "geno", cutoff = 0.25, quiet = FALSE, freq = FALSE)
 
-##output to genalex file 
-genind2genalex(QUAC_genind_nomd,file="QUAC_data_frames\\QUAC_clone_free_md_free.csv")
+##output to genalex file if needed 
+#genind2genalex(QUAC_genind_nomd,file="QUAC_data_frames\\QUAC_clone_free_md_free.csv")
 ##open genalex and conver to arp file to load new genind file 
 #back into R, and then also save as a data frame for 
 #relatedness analyses
@@ -82,7 +80,7 @@ QUAC_halfsib_names_cleanfront <- gsub("^.*\\.","", QUAC_halfsib_names)
 
 QUAC_halfsib_names_cleanback <- gsub("^.*\\_","", QUAC_halfsib_names_cleanfront)
 
-QUAC_relate_ind_remove <- unique(QUAC_halfsib_names_cleanback)
+QUAC_relate_ind_remove <- unique(QUAC_halfsib_names_cleanback) ##260 of these individuals have > 25% relatedness
 
 ##now limit genind by these names 
 QUAC_relate_red_gen <- QUAC_genind_nomd[!rownames(QUAC_genind_nomd@tab) %in% QUAC_relate_ind_remove,]

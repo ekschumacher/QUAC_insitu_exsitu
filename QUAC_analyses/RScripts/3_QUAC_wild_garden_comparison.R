7771d1251e13ef36006e88d5d7189fdca483f302
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2021-12-02 14:31:22 -0600

    Changing slashes, updating file pathways to include dots, and rewording with object instead of file for genind objects

--- QUAC_analyses/RScripts/3_QUAC_wild_garden_comparison.R
+++ QUAC_analyses/RScripts/3_QUAC_wild_garden_comparison.R
@@ -12,39 +12,33 @@ library(diveRsity)
 library(poppr)
 library(hierfstat)
 
-#####################################
-############ Directories ############
-#####################################
-QUAC_data_files <- "C:\\Users\\eschumacher\\Documents\\GitHub\\QUAC_insitu_exsitu\\QUAC_data_files"
-
-QUAC_analysis_results <- "C:\\Users\\eschumacher\\Documents\\GitHub\\QUAC_insitu_exsitu\\QUAC_analyses\\Results"
-
 ################################
 ########## Load files ##########
 ################################
-setwd(QUAC_data_files)
+setwd("../../QUAC_data_files")
 
-##convert to a genind if not already converted 
-#arp2gen(paste0(QUAC_data_files, "\\QUAC_genind\\QUAC_garden_wild_clean.arp"))
+##convert to a genepop file, if necessary
+#arp2gen(paste0(QUAC_data_files, "/QUAC_genind/QUAC_garden_wild_clean.arp"))
 
-##load in genind for QUAC
-QUAC_garden_wild_gen <- read.genepop(paste0(QUAC_data_files, "\\QUAC_genind\\QUAC_garden_wild_clean.gen"), ncode = 3)
+##load in genepop file as a genind object 
+QUAC_garden_wild_gen <- read.genepop("QUAC_adegenet_files/Garden_Wild/QUAC_garden_wild_clean.gen", ncode = 3)
 
 ##load in data frame 
-QUAC_garden_wild_df <- read.csv(paste0(QUAC_data_files, "\\QUAC_data_frames\\QUAC_garden_wild_clean_df.csv"))
+QUAC_garden_wild_df <- read.csv("QUAC_data_frames/QUAC_garden_wild_clean_df.csv")
 
-##rename individuals in the genind file
+##rename individuals in genind object
 rownames(QUAC_garden_wild_gen@tab) <- QUAC_garden_wild_df$Ind
 
-##rename population names 
+##rename population names in the genind object
+#create pop name list
 QUAC_popnames <- unique(QUAC_garden_wild_df$Pop)
-
+#rename populations in the genind object
 levels(QUAC_garden_wild_gen@pop) <- QUAC_popnames
 
-##load in get allele category function
-source("C:\\Users\\eschumacher\\Documents\\GitHub\\QUAC_insitu_exsitu\\QUAC_analyses\\RScripts\\Fa_sample_funcs.R")
+##load in function to calculate allele frequency categories
+source("../QUAC_analyses/RScripts/Fa_sample_funcs.R")
 
-##load in functions
+##create functions to run code 
 colMax <- function(data) sapply(data, max, na.rm = TRUE)
 sample.pop<-function(genind_obj,vect_pop_ID,vect_samp_sizes){
   p<-length(vect_pop_ID)
@@ -60,12 +54,13 @@ sample.pop<-function(genind_obj,vect_pop_ID,vect_samp_sizes){
 ############################################################
 ############ Comparing wild and garden populations #########
 ############################################################
+setwd("../QUAC_analyses/Results/Wild_Garden_Comparison")
 ##calculate genetic diversity statistics 
 QUAC_wild_hexp <- summary(seppop(QUAC_garden_wild_gen)$Wild)[7]
 QUAC_garden_hexp <- summary(seppop(QUAC_garden_wild_gen)$Garden)[7]
 
 ##calculate allelic richness
-QUAC_garden_allrich <- allelic.richness(seppop(QUAC_garden_wild_gen)$Wild)$Ar
+QUAC_wild_allrich <- allelic.richness(seppop(QUAC_garden_wild_gen)$Wild)$Ar
 QUAC_garden_allrich <- allelic.richness(seppop(QUAC_garden_wild_gen)$Garden)$Ar
 
 ##create gendiv data frame 
@@ -109,7 +104,7 @@ QUAC_garden_wild_results[1,1] <- mean(QUAC_garden_hexp$Hexp) ; QUAC_garden_wild_
 QUAC_garden_wild_results[2,1] <- mean(QUAC_garden_allrich[,1]) ; QUAC_garden_wild_results[2,2] <- mean(QUAC_wild_allrich[,1])
 
 ##write out table 
-write.csv(QUAC_garden_wild_results, paste0(QUAC_analysis_results, "\\Wild_Garden_Comparison\\gendiv_dif.csv"))
+write.csv(QUAC_garden_wild_results, "QUAC_garden_wild_gendiv_dif_df.csv")
 
 ####################################
 #### Allelic capture code ##########
@@ -117,7 +112,7 @@ write.csv(QUAC_garden_wild_results, paste0(QUAC_analysis_results, "\\Wild_Garden
 ##list out allele categories
 list_allele_cat<-c("global","glob_v_com","glob_com","glob_lowfr","glob_rare","reg_rare","loc_com_d1","loc_com_d2","loc_rare")
 
-##create matrices to store results
+##create matrices to store allele frequency/allele capture code 
 QUAC_all_exist <- matrix(nrow = 1, ncol = 9)
 QUAC_wild_capt <- matrix(nrow = 1, ncol = 10)
 
@@ -125,13 +120,13 @@ QUAC_wild_capt <- matrix(nrow = 1, ncol = 10)
 QUAC_seppop <- seppop(QUAC_garden_wild_gen)
 
 ##calculate number of individuals per pop
-n_ind_p_pop <- table(QUAC_seppop[[2]])
+n_ind_p_pop <- as.numeric(table(QUAC_seppop[[2]]@pop))
 
-##convert to genpop
+##convert to a genpop object
 QUAC_wild_genpop <- genind2genpop(QUAC_seppop[[2]])
 
-# allele_cat<-get.allele.cat(Spp_genpop, region_makeup, 2, n_ind_p_pop,n_drop=n_to_drop)	
-QUAC_allele_cat <- get.allele.cat(QUAC_wild_genpop, 1, 1, as.numeric(n_ind_p_pop[2]), glob_only = TRUE)	
+##calculate 	
+QUAC_allele_cat <- get.allele.cat(QUAC_wild_genpop, 1, 1, as.numeric(n_ind_p_pop), glob_only = TRUE)	
 
 ##create alleles captured by gardens 
 n_ind_W<-table(QUAC_garden_wild_gen@pop)[2];  n_ind_G<-table(QUAC_garden_wild_gen@pop)[1]; 
@@ -149,14 +144,14 @@ QUAC_allele_cap_table <- matrix(nrow = 1, ncol = length(list_allele_cat))
 ##add rownames and colnames 
 colnames(QUAC_allele_cap_table) <- list_allele_cat
 
-##run loop to generate table 
+##run loop to generate allelic capture table 
 for(m in 1:length(QUAC_all_exist[1,])){
     
   QUAC_allele_cap_table[,m] <- paste0(signif((QUAC_wild_capt[,m]*100),3), "%", "(", signif(QUAC_all_exist[,m],3), ")")
 
 }
 
-write.csv(QUAC_allele_cap_table, paste0(QUAC_analysis_results, "\\Wild_Garden_Comparison\\QUAC_allele_cap_table.csv"))
+write.csv(QUAC_allele_cap_table, "QUAC_allele_cap_df.csv")
 
 ##################################
 ### Replicate Allele Capture #####
@@ -189,7 +184,7 @@ rownames(QUAC_wild_cap_all_dup_df) <- c("One Copy", "> 1 Copy", "> 2 Copies", ">
                                         "> 9 Copies", "> 10 Copies")
 
 ##write out duplicate table 
-write.csv(QUAC_wild_cap_all_dup_df, paste0(QUAC_analysis_results, "\\Wild_Garden_Comparison\\QUAC_wild_cap_all_dup_df.csv"))
+write.csv(QUAC_wild_cap_all_dup_df, "QUAC_wild_cap_all_dup_df.csv")
 
 ##write out session info
 sessionInfo()

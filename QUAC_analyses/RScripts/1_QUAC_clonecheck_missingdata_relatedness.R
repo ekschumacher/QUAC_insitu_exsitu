ece10d2f1a67d3093a90bef5f44ab851d676a246
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2021-12-03 11:26:36 -0600

    Adding the correct files to repos and updating code with more cconsistent naming, slashes, correcting some code errors

--- QUAC_analyses/RScripts/1_QUAC_clonecheck_missingdata_relatedness.R
+++ QUAC_analyses/RScripts/1_QUAC_clonecheck_missingdata_relatedness.R
@@ -13,44 +13,37 @@ library(adegenet)
 library(poppr)
 library(Demerelate)
 
-#####################################
-############ Directories ############
-#####################################
-QUAC_data_files <- "C:/Users/eschumacher/Documents/GitHub/QUAC_insitu_exsitu/QUAC_data_files"
-
-QUAC_analysis_results <- "C:/Users/eschumacher/Documents/GitHub/QUAC_insitu_exsitu/QUAC_analyses/Results"
-
 ##############################################
 ############# Convert to Genind ##############
 ##############################################
 ##set working directory to load in data files 
-setwd(QUAC_data_files)
+setwd("../../QUAC_data_files")
 
 ##convert QUAC all pop arlequin file into genepop file if needed
-#arp2gen(paste0(QUAC_data_files, "/QUAC_genind/quac_allpop.arp"))
+#arp2gen(paste0(QUAC_data_files, "/QUAC_adegenet_files/QUAC_rawscores_allpop.arp"))
 
 ##now read in genepop file as a genind for adegenet 
-QUAC_allpop_gen <- read.genepop("QUAC_genind/quac_allpop.gen", ncode = 3)
+QUAC_garden_wild_gen <- read.genepop("QUAC_adegenet_files/QUAC_rawscores_garden_wild.gen", ncode = 3)
  
+##load relatedness data frame for relatedness analysis 
+QUAC_rel_df <- read.csv("QUAC_data_frames/QUAC_rawscores_garden_wild.csv")
+
 ##load data frame - used for labeling population and individual names 
-QUAC_allpop_df <- read.csv("QUAC_data_frames/QUAC_allpop.csv")
+#QUAC_allpop_df <- read.csv("QUAC_data_frames/QUAC_allpop.csv")
 
 ##rename individuals in the genind object 
-rownames(QUAC_allpop_gen@tab) <- QUAC_allpop_df$ID
+rownames(QUAC_garden_wild_gen@tab) <- QUAC_rel_df$ID
 
 ##create population name list and rename the populations in the genind object 
-QUAC_popnames <- unique(QUAC_allpop_df$POP)
-levels(QUAC_allpop_gen@pop) <- QUAC_popnames
-
-##load relatedness data frame for relatedness analysis 
-QUAC_rel_df <- read.csv("QUAC_data_frames/QUAC_rel_df.csv")
+QUAC_popnames <- unique(QUAC_rel_df$POP)
+levels(QUAC_garden_wild_gen@pop) <- QUAC_popnames
 
 #######################################################################
 ############# Clone check, relatedness, and missing data ##############
 #######################################################################
 ####run clone check 
 ##convert to genelcone object
-QUAC_geneclone <- as.genclone(QUAC_allpop_gen)
+QUAC_geneclone <- as.genclone(QUAC_garden_wild_gen)
 ##identify multi-locus genotypes (non-clones)
 QUAC_gen_id <- mlg.id(QUAC_geneclone)
 ##function to pull out all clones into a list
@@ -58,20 +51,26 @@ QUAC_clone_index <- which(sapply(QUAC_gen_id,function(x) length(x)>1))
 ##now remove clones from the matrix 
 QUAC_noclones <- clonecorrect(QUAC_geneclone)
 ##convert back to a genind object
-QUAC_genind_nocl <- genclone2genind(QUAC_noclones) ##left with 456 individuals 
+QUAC_genind_nocl <- genclone2genind(QUAC_noclones) ##left with 456 individuals, there were 7 clones 
 
 ##remove individuals with too much missing data 
-QUAC_genind_nomd <- missingno(QUAC_genind_nocl, type = "geno", cutoff = 0.25, quiet = FALSE, freq = FALSE)
+QUAC_genind_nomd <- missingno(QUAC_genind_nocl, type = "geno", cutoff = 0.25, quiet = FALSE, freq = FALSE) ##there were 7 individuals with too much missing data, left with 449 individuals
 
 ##output to genalex file if needed 
-#genind2genalex(QUAC_genind_nomd,file="QUAC_data_frames/QUAC_clone_free_md_free.csv")
+#genind2genalex(QUAC_genind_nomd, file="QUAC_data_frames/QUAC_clone_free_md_free.csv", 
+#               overwrite = TRUE)
 ##open genalex and convert to arp file to load new genepop into R  
+##remove the individuals from the relatedness df that have been removed for missing data and clones
+QUAC_nomd_nocl_rel_df <- QUAC_rel_df[QUAC_rel_df$ID %in% rownames(QUAC_genind_nomd@tab),]
+
+##write out data frame 
+write.csv(QUAC_nomd_nocl_rel_df, "QUAC_data_frames/Garden_Wild/QUAC_garden_wild_clean_df.csv")
 
 ###################################
 ###### Relatedness Analysis #######
 ###################################
 ##run relatedness analysis
-QUAC_relatedness_df <- Demerelate(QUAC_rel_df, object = T, value = "loiselle")
+QUAC_relatedness_df <- Demerelate(QUAC_nomnd_nocl_rel_df, object = T, value = "loiselle")
 ##now identify how many individuals have greater than 25% relatedness = half siblings
 QUAC_halfsib_names <- names(which(unlist(QUAC_relatedness_df$Empirical_Relatedness) > 0.25))
 
@@ -85,10 +84,10 @@ QUAC_relate_ind_remove <- unique(QUAC_halfsib_names_cleanback) ##260 of these in
 ##now limit genind object by these names 
 QUAC_relate_red_gen <- QUAC_genind_nomd[!rownames(QUAC_genind_nomd@tab) %in% QUAC_relate_ind_remove,]
 ##and export to genalex
-genind2genalex(QUAC_relate_red_gen, file="QUAC_data_frames/Relate_Red/QUAC_relate_red_gen.csv",
+genind2genalex(QUAC_relate_red_gen, file="QUAC_data_frames/Relate_Red/QUAC_relate_red_genalex.csv",
                overwrite = TRUE)
 ##limit data frame with the indiviudals that are highly related  
-QUAC_red_relate_df <- QUAC_rel_df[!QUAC_rel_df$Sample.ID %in% QUAC_relate_ind_remove,]
+QUAC_red_relate_df <- QUAC_rel_df[!QUAC_rel_df$ID %in% QUAC_relate_ind_remove,]
 ##write out data frame  
 write.csv(QUAC_red_relate_df, "QUAC_data_frames/Relate_Red/QUAC_red_relate_df.csv")
 
--- QUAC_analyses/RScripts/4_QUAC_PCoA.R
+++ QUAC_analyses/RScripts/4_QUAC_PCoA.R
@@ -10,20 +10,13 @@ library(adegenet)
 library(diveRsity)
 
 #####################################
-############ Directories ############
+########## Load in Files ############
 #####################################
-##set directory to all butternut files 
-QUAC_data_files <- "C:\\Users\\eschumacher\\Documents\\GitHub\\QUAC_insitu_exsitu\\QUAC_data_files"
-
-QUAC_analysis_results <- "C:\\Users\\eschumacher\\Documents\\GitHub\\QUAC_insitu_exsitu\\QUAC_analyses\\Results"
-
-########################################
-########### Load in Files ##############
-########################################
+setwd("../../QUAC_data_files")
 ##first, load in reduced genind with no half siblings
-QUAC_red_relate_gen <- read.genepop(paste0(QUAC_data_files, "\\QUAC_genind\\Relate_Red\\QUAC_relate_red.gen"), ncode = 3)
+QUAC_red_relate_gen <- read.genepop("QUAC_adegenet_files/Relate_Red/QUAC_relate_red_garden_wild.gen", ncode = 3)
 ##load in data frame 
-QUAC_red_relate_df <- read.csv(paste0(QUAC_data_files, "\\QUAC_data_frames\\Relate_Red\\QUAC_relate_red_df.csv"))
+QUAC_red_relate_df <- read.csv("QUAC_data_frames/Relate_Red/QUAC_red_relate_allpop_df.csv")
 ###rename individuals 
 rownames(QUAC_red_relate_gen@tab) <- QUAC_red_relate_df$Ind
 ##create population document 
@@ -32,26 +25,26 @@ QUAC_pop_names <- unique(QUAC_red_relate_df$Pop)
 levels(QUAC_red_relate_gen@pop) <- QUAC_pop_names
 
 ###load files to compare wild and garden individuals 
-QUAC_garden_wild_gen <- read.genepop(paste0(QUAC_data_files, "\\QUAC_genind\\Relate_Red\\QUAC_relate_red_garden_wild.gen"), ncode = 3)
+QUAC_garden_wild_gen <- read.genepop(paste0(QUAC_data_files, "/QUAC_genind/Relate_Red/QUAC_relate_red_garden_wild.gen"), ncode = 3)
 ##load file to name pops and inds
-QUAC_garden_wild_df <- read.csv(paste0(QUAC_data_files, "\\QUAC_data_frames\\Relate_Red\\QUAC_relate_red_garden_wild_df.csv"))
+QUAC_garden_wild_df <- read.csv(paste0(QUAC_data_files, "/QUAC_data_frames/Relate_Red/QUAC_relate_red_garden_wild_df.csv"))
 ##name inds and pops
 rownames(QUAC_garden_wild_gen@tab) <- QUAC_garden_wild_df$Ind
 levels(QUAC_garden_wild_gen@pop) <- unique(QUAC_garden_wild_df$Pop)
 
 ######load files for all wild pops compared to garden 
-QUAC_garden_allwildpop_gen <- read.genepop(paste0(QUAC_data_files, "\\QUAC_genind\\Relate_Red\\QUAC_relate_red_garden_allwildpop.gen"), ncode = 3)
+QUAC_garden_allwildpop_gen <- read.genepop(paste0(QUAC_data_files, "/QUAC_genind/Relate_Red/QUAC_relate_red_garden_allwildpop.gen"), ncode = 3)
 ##load data frame 
-QUAC_garden_allwildpop_df <- read.csv(paste0(QUAC_data_files, "\\QUAC_data_frames\\Relate_Red\\QUAC_relate_red_garden_allwildpop_df.csv"))
+QUAC_garden_allwildpop_df <- read.csv(paste0(QUAC_data_files, "/QUAC_data_frames/Relate_Red/QUAC_relate_red_garden_allwildpop_df.csv"))
 ##rename rows and pops 
 rownames(QUAC_garden_allwildpop_gen@tab) <- QUAC_garden_allwildpop_df$Ind
 levels(QUAC_garden_allwildpop_gen@pop) <- c("Garden","Porter","Magazine","Pryor","Sugarloaf","Kessler")
 
 #####load files for just wild PCA
 ##genind
-QUAC_wildpop_gen <- read.genepop(paste0(QUAC_data_files, "\\QUAC_genind\\Relate_Red\\QUAC_relate_red_allwildpop.gen"), ncode = 3)
+QUAC_wildpop_gen <- read.genepop(paste0(QUAC_data_files, "/QUAC_genind/Relate_Red/QUAC_relate_red_allwildpop.gen"), ncode = 3)
 ##data frame 
-QUAC_wildpop_df <- read.csv(paste0(QUAC_data_files, "\\QUAC_data_frames\\Relate_Red\\QUAC_relate_red_allwildpop_df.csv"))
+QUAC_wildpop_df <- read.csv(paste0(QUAC_data_files, "/QUAC_data_frames/Relate_Red/QUAC_relate_red_allwildpop_df.csv"))
 ##rename with rows and individual names 
 rownames(QUAC_wildpop_gen@tab) <- QUAC_wildpop_df$Ind
 levels(QUAC_wildpop_gen@pop) <- c("Porter","Magazine","Pryor","Sugarloaf","Kessler")
@@ -81,7 +74,7 @@ for(i in 1:length(QUAC_pop_names)){
 allpop_pc1 <- signif(((QUAC_pca$eig[1])/sum(QUAC_pca$eig))*100, 3)
 allpop_pc2 <- signif(((QUAC_pca$eig[2])/sum(QUAC_pca$eig))*100, 3)
 ##create pdf of all pops 
-pdf(paste0(QUAC_analysis_results, "\\Clustering\\QUAC_allpop_pca.pdf"), width = 10, height = 8)
+pdf(paste0(QUAC_analysis_results, "/Clustering/QUAC_allpop_pca.pdf"), width = 10, height = 8)
 ##create a diagram of all populations 
 plot(QUAC_allpop_pca_df[,1], QUAC_allpop_pca_df[,2], pch = 17, col = QUAC_allpop_pca_df[,4], ylim = c(-2,2), xlim = c(-2,3),
      xlab = paste0('PC1 (',allpop_pc1, "%)"), ylab = paste0("PC2 (", allpop_pc2, "%)"))
@@ -98,7 +91,7 @@ QUAC_garden_wild_tab <- tab(QUAC_garden_wild_gen, freq=TRUE, NA.method="mean")
 #now run PCA
 QUAC_garden_wild_pca <- dudi.pca(QUAC_garden_wild_tab, scale = FALSE, scannf = FALSE, nf = 2)
 ##create pdf 
-pdf(paste0(QUAC_analysis_results, "\\Clustering\\QUAC_garden_wild_pca.pdf"), width = 10, height = 8)
+pdf(paste0(QUAC_analysis_results, "/Clustering/QUAC_garden_wild_pca.pdf"), width = 10, height = 8)
 s.class(QUAC_garden_wild_pca$li, fac=pop(QUAC_garden_wild_gen),
         col=c("darkseagreen3", "forestgreen"),
         axesel=FALSE, cstar=0, cpoint=2, clabel = 1, addaxes = TRUE)
@@ -112,7 +105,7 @@ QUAC_garden_allwildpop_pca <- dudi.pca(QUAC_garden_allwildpop_tab, scannf = 2, s
 ##create color vector
 QUAC_colors <- c("darkslategray3","dodgerblue2","darkgreen","darkorchid","lightcoral", "cadetblue4")
 ##write out PCoA 
-pdf(paste0(QUAC_analysis_results, "\\Clustering\\QUAC_garden_allwildpop_pca.pdf"))
+pdf(paste0(QUAC_analysis_results, "/Clustering/QUAC_garden_allwildpop_pca.pdf"))
 s.class(QUAC_pca$li, fac=pop(QUAC_garden_allwildpop_gen),
        col=QUAC_colors,
         axesel=FALSE, cstar=0, cpoint=2, clabel = 1, addaxes = TRUE)
@@ -128,7 +121,7 @@ wildpop_pc1 <- signif(((QUAC_wildpop_pca$eig[1])/sum(QUAC_wildpop_pca$eig))*100,
 wildpop_pc2 <- signif(((QUAC_wildpop_pca$eig[2])/sum(QUAC_wildpop_pca$eig))*100, 3)
 
 ##write out data file 
-pdf(paste0(QUAC_analysis_results, "\\Clustering\\QUAC_wildpop_pca.pdf"), width = 10, height = 8)
+pdf(paste0(QUAC_analysis_results, "/Clustering/QUAC_wildpop_pca.pdf"), width = 10, height = 8)
 s.class(QUAC_wildpop_pca$li, fac = pop(QUAC_wildpop_gen),
         col = QUAC_colors[c(2:6)], 
         axesel=FALSE, cstar=0, cpoint=2, clabel = 1, addaxes = TRUE)
